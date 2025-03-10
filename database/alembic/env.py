c1e27dc4da61b64bf4ef5329da843cbd9ca95863
Fitzfoufou <francois.fitzpatrick@gmail.com>
2025-03-10 13:43:37 +0100

    feat (alembic/env.py): setup alembic config with env database_url

--- database/alembic/env.py
+++ database/alembic/env.py
@@ -2,8 +2,12 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlalchemy import create_engine
 
 from alembic import context
+
+from database.database import Base
+from database.database import DATABASE_URL
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -15,10 +19,9 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+engine = create_engine(DATABASE_URL)
+target_metadata = Base.metadata
+config.set_section_option('alembic', 'sqlalchemy.url', DATABASE_URL)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
