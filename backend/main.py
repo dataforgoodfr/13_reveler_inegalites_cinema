43a3c0d5a8bc2e1b981c9eed540ffbc31c7480e0
Fitzfoufou <francois.fitzpatrick@gmail.com>
2025-04-11 15:30:19 +0200

    feat (routers/film.py): add route for fiche film

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from backend.routers import film
 
 backend = FastAPI()
 
 @backend.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+ 
+# Mount routers
+backend.include_router(film.router)
