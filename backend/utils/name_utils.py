92f4231c243348ee8d85ca5b96fcca8abbb559fb
Fitzfoufou <francois.fitzpatrick@gmail.com>
2025-05-02 12:55:09 +0200

    feat (name_utils.py): adding class to analyse gender from name

---
+++ backend/utils/name_utils.py
@@ -0,0 +1,21 @@
+import gender_guesser.detector as gender
+import re
+
+_gender_detector = gender.Detector()
+
+def split_full_name(full_name: str) -> tuple:
+    parts = full_name.strip().split()
+    if len(parts) == 0:
+        return ("", "")
+    elif len(parts) == 1:
+        return (parts[0], "")
+    else:
+        return (parts[0], parts[-1])
+
+def guess_gender(first_name: str) -> str:
+    return _gender_detector.get_gender(first_name)
+
+def remove_extra_spaces(s: str) -> str:
+    if not isinstance(s, str):
+        raise ValueError("Input must be a string", s)
+    return re.sub(r'\s+', ' ', s).strip()
