df3926ab34f36c6d71adf22e910af42ed22f57d0
Fitzfoufou <francois.fitzpatrick@gmail.com>
2025-05-09 13:03:55 +0200

    feat (metabase route): adding route to access metabase dashboard

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.routers import film, festival
+from backend.routers import film, festival, metabase
 
 backend = FastAPI()
 
@@ -20,6 +20,7 @@ def read_root():
 # Mount routers
 backend.include_router(film.router)
 backend.include_router(festival.router)
+backend.include_router(metabase.router)
 
 for route in backend.routes:
     print(f"{route.path} -> {route.name}")
