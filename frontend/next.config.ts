681d8e153d35292b1404b84c492224daf988a79c
Florian_Yun <fyun@oxyl.fr>
2025-05-10 12:19:25 +0200

    [FEAT] Adding statistics page

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,17 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   distDir: "dist",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "fr.web.img6.acsta.net",
-        port: "",
-        pathname: "/img/29/eb/**",
-        search: "",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
