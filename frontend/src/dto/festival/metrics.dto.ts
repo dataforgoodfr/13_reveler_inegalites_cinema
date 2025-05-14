f1eeeb6aa01466fcc4ecdcd4668dc09961ac83f4
Florian_Yun <fyun@oxyl.fr>
2025-05-14 13:29:38 +0200

    [FIX] Change naming

--- frontend/src/dto/festival/metrics.dto.ts
+++ frontend/src/dto/festival/metrics.dto.ts
@@ -1,4 +1,4 @@
 export type Metrics = {
   female_representation_in_nominated_films: number;
-  female_representation_in_winner_price: number;
+  female_representation_in_award_winning_films: number;
 }
