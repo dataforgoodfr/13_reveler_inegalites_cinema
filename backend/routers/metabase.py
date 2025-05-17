771311fb1697b664abbf66e8533e84fca1cd70eb
Fitzfoufou <francois.fitzpatrick@gmail.com>
2025-05-17 13:28:30 +0200

    fix (metabase.py): put env vars in route to be get at run time

--- backend/routers/metabase.py
+++ backend/routers/metabase.py
@@ -7,16 +7,16 @@ import os
 
 router = APIRouter()
 
-METABASE_SITE_URL = os.getenv("METABASE_SITE_URL")
-METABASE_SECRET_KEY = os.getenv("METABASE_SECRET_KEY")
-METABASE_DASHBOARD_ID = os.getenv("METABASE_DASHBOARD_ID")
-
 class EmbedRequest(BaseModel):
     dashboard_id: int
     params: Optional[Dict[str, str]] = None
 
 @router.get("/metabase/iframe-url")
 def get_fixed_metabase_iframe_url():
+    METABASE_SITE_URL = os.getenv("METABASE_SITE_URL")
+    METABASE_SECRET_KEY = os.getenv("METABASE_SECRET_KEY")
+    METABASE_DASHBOARD_ID = os.getenv("METABASE_DASHBOARD_ID")
+
     if not METABASE_SECRET_KEY or not METABASE_SITE_URL or not METABASE_DASHBOARD_ID:
         raise HTTPException(status_code=503, detail="Metabase config missing")
 
