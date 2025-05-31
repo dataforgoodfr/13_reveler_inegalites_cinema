ae21b175d767bb521438d0e17ae243db61bc05c7
Florian_Yun <fyun@oxyl.fr>
2025-05-31 16:46:12 +0200

    [FRONT] [FIX] Home phrase accroche Typo et passage a la ligne

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -13,20 +13,23 @@ export default function Home() {
         }
       ></div>
       <main className="container mx-auto px-4 py-16 flex flex-col items-center justify-center text-center text-white">
-        <div className=" animate-slide-up">
+        <div className="animate-slide-up">
           <div className="mt-30 mb-12">
             <span className="text-5xl md:text-6xl font-bold w-3/4">
               <span className="text-violet-500">29%</span>
               <span>
                 {" "}
-                des réalisateur·rices actifs·ves ces 10 dernières années sont
-                des{" "}
+                des réalisateurs·rices actifs·ves
+                <br className="hidden md:block" />
+                ces 10 dernières années sont des{" "}
               </span>
               <span className="text-violet-500">femmes</span>
             </span>
           </div>
           <Link href="/statistics">
-            <Button className="bg-violet-500 cursor-pointer">Voir les statistiques</Button>
+            <Button className="bg-violet-500 cursor-pointer">
+              Voir les statistiques
+            </Button>
           </Link>
         </div>
       </main>
