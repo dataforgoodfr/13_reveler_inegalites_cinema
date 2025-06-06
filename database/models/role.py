24c758b0420fc8eb7cae48973a79b7bef4863ee1
Fitzfoufou <francois.fitzpatrick@gmail.com>
2025-06-06 17:54:39 +0200

    feat (role_constants.py): adding inclusive name in roles
    #119

--- database/models/role.py
+++ database/models/role.py
@@ -9,5 +9,6 @@ class Role(Base):
     name = Column(String, nullable=False)
     allocine_name = Column(String, nullable=True)
     is_key_role = Column(Boolean, default=False, nullable=False)
+    inclusive_name = Column(String, nullable=True)
 
     film_credits = relationship("FilmCredit", back_populates="role")
