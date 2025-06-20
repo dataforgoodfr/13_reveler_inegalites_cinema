3ca2e62ff612d413256074e7e046f2d42b18388d
Fitzfoufou <francois.fitzpatrick@gmail.com>
2025-06-20 19:26:38 +0200

    fix (routers/festival.py): fix bug with awards not updating
    #113

--- backend/routers/festival.py
+++ backend/routers/festival.py
@@ -13,11 +13,11 @@ get_db_dep = Depends(get_db)
 def get_festival(
     festival_id: int,
     year: Optional[int] = Query(default=None),
-    award_id: Optional[int] = Query(default=None),
+    award: Optional[int] = Query(default=None),
     db: Session = get_db_dep
 ):
     use_case = GetFestivalDetails(db)
-    festival_data = use_case.execute(festival_id, year=year, award_id=award_id)
+    festival_data = use_case.execute(festival_id, year=year, award_id=award)
 
     if "error" in festival_data:
         raise HTTPException(status_code=404, detail=festival_data["error"])
