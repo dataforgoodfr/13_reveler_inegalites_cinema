9450fc4c494315901d759b327a94d54b58b8b40d
Fitzfoufou <francois.fitzpatrick@gmail.com>
2025-06-20 14:58:13 +0200

    feat (festival_awards.py): adding new label attributes
    #116

--- database/models/festival_award.py
+++ database/models/festival_award.py
@@ -6,7 +6,9 @@ class FestivalAward(Base):
     __tablename__ = "ric_festival_awards"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    mubi_label = Column(String, nullable=False)
+    english_label = Column(String, nullable=True)
+    french_label = Column(String, nullable=True)
     festival_id = Column(Integer, ForeignKey("ric_festivals.id"), nullable=False)
 
     festival = relationship("Festival", back_populates="awards")
