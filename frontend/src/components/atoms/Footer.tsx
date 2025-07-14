bbd9a3026da7e82fc9559dd0b685d1be9cba1ad1
Florian Yun <yunflorian@hotmail.fr>
2025-07-14 18:45:57 +0200

    ui (Footer.tsx): Footer V5

--- frontend/src/components/atoms/Footer.tsx
+++ frontend/src/components/atoms/Footer.tsx
@@ -9,22 +9,38 @@ const Footer = () => {
 const isMobile: boolean = useMediaQuery("(max-width: 768px)");
 
   return (
-    <div className="px-5 md:px-15 py-5 w-full flex flex-col md:flex-row gap-10 bg-violet-700 text-white">
+    <div className="px-5 md:px-15 py-5 w-full flex flex-col md:flex-row gap-10 bg-[#421F7A] text-white">
       <div className="flex flex-col md:flex-row gap-20 w-full justify-around">
+        {
+          !isMobile &&
+          <div className="flex flex-col justify-around w-full">
+            <Image
+              src="/logo_5050.png"
+              className="w-full"
+              alt=""
+              width={500}
+              height={100}
+            />
+            <Image src={"/logo_d4g.png"} alt="" width={70} height={0} />
+          </div>
+        }
         <div className="flex flex-col gap-5 md:gap-3 justify-center w-full">
-          <Image
-            src="/logo_5050.png"
-            className="w-3/4 md:w-full"
-            alt=""
-            width={500}
-            height={100}
-          />
+          {
+            isMobile &&
+            <Image
+              src="/logo_5050.png"
+              className="w-3/4 md:w-full"
+              alt=""
+              width={500}
+              height={100}
+            />
+          }
           <h1 className="font-bold">Collectif 50/50</h1>
-          <a href="https://collectif5050.com" target="_blank" rel="noopener noreferrer" className="flex hover:underline">
+          <a href="https://collectif5050.com" target="_blank" rel="noopener noreferrer" className="flex text-[#BABFD6] hover:underline hover:text-white">
             <p className="pr-2">Site internet</p>
             <ExternalLink />
           </a>
-          <a href="https://collectif5050.com/nos-etudes/" target="_blank" rel="noopener noreferrer" className="flex hover:underline">
+          <a href="https://collectif5050.com/nos-etudes/" target="_blank" rel="noopener noreferrer" className="flex text-[#BABFD6] hover:underline hover:text-white">
             <p className="pr-2">Nos études</p>
             <ExternalLink />
           </a>
@@ -48,37 +64,40 @@ const isMobile: boolean = useMediaQuery("(max-width: 768px)");
             </a>
           </div>
           <a href="https://www.helloasso.com/associations/collectif-50-50" target="_blank" rel="noopener noreferrer" className="w-full">
-          <Button className="text-black bg-white hover:bg-grey w-full">
+          <Button className="text-black bg-white hover:bg-grey w-full cursor-pointer">
             Adhérer / Faire un don
           </Button>
           </a>
         </div>
-        <div className="flex flex-col gap-5 md:gap-1 justify-center w-full pb-10">
-          <Image src={"/logo_d4g.png"} alt="" width={70} height={0} />
+        <div className="flex flex-col gap-5 md:gap-1 w-full justify-between">
+          {
+            isMobile &&
+            <Image src={"/logo_d4g.png"} alt="" width={70} height={0} />
+          }
           <h1 className="font-bold">Data for good</h1>
-          <a href="https://dataforgood.fr/" target="_blank" rel="noopener noreferrer" className="flex hover:underline">
+          <a href="https://dataforgood.fr/" target="_blank" rel="noopener noreferrer" className="flex text-[#BABFD6] hover:underline hover:text-white">
             <p className="pr-2">Site internet</p>
             <ExternalLink />
           </a>
-          <a href="https://x.com/dataforgood_fr?lang=fr" target="_blank" rel="noopener noreferrer" className="flex hover:underline">
+          <a href="https://x.com/dataforgood_fr?lang=fr" target="_blank" rel="noopener noreferrer" className="flex text-[#BABFD6] hover:underline hover:text-white">
             <p className="pr-2">Twitter</p>
             <ExternalLink />
           </a>
           <a
-            href="https://www.linkedin.com/company/dataforgood" target="_blank" rel="noopener noreferrer" className="flex hover:underline">
+            href="https://www.linkedin.com/company/dataforgood" target="_blank" rel="noopener noreferrer" className="flex text-[#BABFD6] hover:underline hover:text-white">
             <p className="pr-2">LinkedIn</p>
             <ExternalLink />
           </a>
-          <a href="mailto:contact@dataforgood.fr" target="_blank" rel="noopener noreferrer" className="flex hover:underline">
+          <a href="mailto:contact@dataforgood.fr" target="_blank" rel="noopener noreferrer" className="flex text-[#BABFD6] hover:underline hover:text-white">
             <p className="pr-2">Envoyer un mail</p>
             <ExternalLink />
           </a>
         </div>
-      </div>
-      <div className="flex justify-center items-center font-bold w-full">
-        <div className="bg-violet-700 border border-white/40 rounded-xl text-center p-4">
-          Observatoire des inégalités dans le cinéma, <br />
-          par le Collectif 50/50 et Data For Good
+        <div className="flex flex-col justify-center w-full">
+          <div className="border border-white/40 rounded-xl text-center p-4">
+            Observatoire des inégalités dans le cinéma, <br />
+            par le Collectif 50/50 et Data For Good
+          </div>
         </div>
       </div>
     </div>
