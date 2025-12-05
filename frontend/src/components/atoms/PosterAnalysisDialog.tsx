8fbd4fd034baeb6027a6038470e17e95d82c2eae
Nicolas Revel <nicolas.revel@laplateforme.io>
2025-12-05 15:56:52 +0100

    task : removing stats dialog content because some datas are not that accurate

--- frontend/src/components/atoms/PosterAnalysisDialog.tsx
+++ frontend/src/components/atoms/PosterAnalysisDialog.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselDots,
-  CarouselItem,
-  CarouselNavigation,
-} from "../ui/carousel";
+import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
 import Image from "next/image";
 import { Drawer, DrawerContent, DrawerHeader, DrawerTitle } from "../ui/drawer";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
@@ -39,7 +33,7 @@ const PosterAnalysisDialog = ({
           }}
         >
           <DrawerHeader className="items-center text-center p-5 h-full">
-            <DrawerTitle className="font-bold text-white">
+            <DrawerTitle className="font-bold text-white pb-4">
               Analyse de l&apos;affiche
             </DrawerTitle>
             <Carousel className="w-full h-full">
@@ -63,51 +57,14 @@ const PosterAnalysisDialog = ({
                       )}
                       <div className="absolute inset-0 bg-black/60"></div>
                       <div className="absolute bottom-0 flex flex-col p-2 text-left font-bold break-words w-full">
-                        <span className="text-violet-500 text-6xl">
-                          {typeof femaleVisibleRatioOnPoster === "number"
-                            ? `${femaleVisibleRatioOnPoster.toPrecision(2)}%`
-                            : "NC"}
-                        </span>
                         <span className="text-white text-l">
-                          Part de personnages perçus comme femmes
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </CarouselItem>
-                <CarouselItem>
-                  <div className="w-full flex justify-center">
-                    <div className="relative flex items-center justify-center bg-[rgba(30, 30, 30, 0.8)] w-[300px]  h-[380px] min-h-[380px]">
-                      {safeImageSource ? (
-                        <Image
-                          loader={() => safeImageSource}
-                          style={{ height: "fit-content" }}
-                          src={safeImageSource}
-                          alt="Affiche"
-                          width={300}
-                          height={0}
-                        />
-                      ) : (
-                        <span className="text-white">
-                          Affiche non disponible
-                        </span>
-                      )}
-                      <div className="absolute inset-0 bg-black/60"></div>
-                      <div className="absolute bottom-0 flex flex-col p-2 text-left font-bold break-words w-full">
-                        <span className="text-violet-500 text-6xl">
-                          {typeof nonWhiteVisibleRatioOnPoster === "number"
-                            ? `${nonWhiteVisibleRatioOnPoster.toPrecision(2)}%`
-                            : "NC"}
-                        </span>
-                        <span className="text-white text-l">
-                          Part de personnages perçus comme non blancs
+                          Statistiques non disponibles.
                         </span>
                       </div>
                     </div>
                   </div>
                 </CarouselItem>
               </CarouselContent>
-              <CarouselDots className="absolute bottom-0 w-full" />
             </Carousel>
           </DrawerHeader>
         </DrawerContent>
@@ -122,7 +79,7 @@ const PosterAnalysisDialog = ({
         style={{ backgroundColor: "rgb(30,30,30)" }}
       >
         <DialogHeader style={{ textAlign: "center", justifyContent: "center" }}>
-          <DialogTitle>Analyse de l&apos;affiche</DialogTitle>
+          <DialogTitle className="font-bold text-white pb-4">Analyse de l&apos;affiche</DialogTitle>
           <div className="w-full flex justify-center">
             <div className="w-3/4">
               <Carousel>
@@ -159,66 +116,15 @@ const PosterAnalysisDialog = ({
                           height={0}
                         />
                       )}
-                      <div className="absolute inset-0 bg-black/60"></div>
+                      <div className="absolute inset-0 bg-black/70"></div>
                       <div className="absolute bottom-0 flex flex-col p-2">
-                        <span className="text-violet-500 text-left text-6xl font-bold">
-                          {typeof femaleVisibleRatioOnPoster === "number"
-                            ? `${femaleVisibleRatioOnPoster.toPrecision(2)}%`
-                            : "NC"}
-                        </span>
-                        <span className="text-white text-left text-l font-bold">
-                          Part de personnages perçus comme femmes
-                        </span>
-                      </div>
-                    </div>
-                  </CarouselItem>
-                  <CarouselItem>
-                    <div
-                      className="relative"
-                      style={{
-                        backgroundColor: "rgba(30, 30, 30, 0.8)",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                      }}
-                    >
-                      {safeImageSource ? (
-                        <Image
-                          loader={() => safeImageSource}
-                          style={{
-                            height: "fit-content",
-                            objectFit: "cover",
-                            maxHeight: "50vh",
-                          }}
-                          src={safeImageSource}
-                          alt="Affiche"
-                          width={300}
-                          height={0}
-                        />
-                      ) : (
-                        <Image
-                          style={{ height: "fit-content" }}
-                          src="/placeholder_image.svg"
-                          alt="Image indisponible"
-                          width={300}
-                          height={0}
-                        />
-                      )}
-                      <div className="absolute inset-0 bg-black/60"></div>
-                      <div className="absolute bottom-0 flex flex-col p-2">
-                        <span className="text-violet-500 text-left text-6xl font-bold">
-                          {typeof nonWhiteVisibleRatioOnPoster === "number"
-                            ? `${nonWhiteVisibleRatioOnPoster.toPrecision(2)}%`
-                            : "NC"}
-                        </span>
-                        <span className="text-white text-left text-l font-bold">
-                          Part de personnages perçus comme non blancs
+                        <span className="text-white text-left text-xl font-bold pb-4 drop-shadow-lg">
+                          Statistiques non disponibles pour le moment.
                         </span>
                       </div>
                     </div>
                   </CarouselItem>
                 </CarouselContent>
-                <CarouselNavigation />
               </Carousel>
             </div>
           </div>
