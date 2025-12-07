ab8ef80f1371fb1d8753f6a7d4587e31b32b7209
Nicolas Revel <nicolas.revel@laplateforme.io>
2025-12-07 18:05:52 +0100

    fix: ajustement de la mise en page et amélioration du rendu des statistiques

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -17,22 +17,15 @@ export default function Home() {
         }
       ></div>
       <main
-        className="container h-full mx-auto px-4 md:pt-96 flex flex-col items-center justify-center text-center text-white"
+        className="container h-full mx-auto px-4 pt-64 md:pt-96 flex flex-col items-center justify-center text-center text-white"
         style={{ background: "unset" }}
       >
         <div className="animate-slide-up flex flex-col items-center gap-8">
-          <div>
-            <span className="text-5xl md:text-6xl text-shadow-sm text-shadow-black font-bold w-3/4">
-              <span>Seules </span>
-              <span className="text-violet-500">29%</span>
-              <span>
-                {" "}
-                des cinéastes de <br className="hidden md:block" />
-                ces dix dernières années sont des{" "}
-              </span>
-              <span className="text-violet-500">femmes</span>
-            </span>
-          </div>
+          <p className="text-5xl md:text-6xl text-shadow-sm text-shadow-black font-bold md:w-5/6 leading-tight">
+            Seules <span className="text-violet-500">29%</span> des cinéastes de
+            ces dix dernières années sont des{" "}
+            <span className="text-violet-500">femmes</span>
+          </p>
           <Button className="bg-violet-500 cursor-pointer" onClick={openSearch}>
             Rechercher par film
           </Button>
